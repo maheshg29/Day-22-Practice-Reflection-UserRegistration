7704a2a7ac31ee3e8a82efa045020ea7d3e8c124
mahesh <gangurdemahesh9@gmail.com>
2022-11-19 09:18:50 +0530

    [mahesh]add all file by using Reflection Concept

---
+++ Day-22-Practice-Reflection-UserRegistration/Program.cs
@@ -0,0 +1,132 @@
+﻿using System.Reflection;
+
+namespace Day_22_Practice_Reflection_UserRegistration
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome TO Day 22 assingment Problem");
+            UserRegistration userRegistration = new UserRegistration();
+
+            Assembly executing = Assembly.GetExecutingAssembly();
+            Type[] types = executing.GetTypes();
+
+            foreach (var item in types)
+            {
+                
+                Console.WriteLine("Class : {0}", item.Name);
+
+        
+                MethodInfo[] methods = item.GetMethods();
+                foreach (var method in methods)
+                {
+                    // Display each method
+                    Console.WriteLine("--> Method : {0}", method.Name);
+
+                    // Array to store parameters
+                    ParameterInfo[] parameters = method.GetParameters();
+                    foreach (var arg in parameters)
+                    {
+                        // Display each parameter
+                        Console.WriteLine("----> Parameter : {0} Type : {1}",
+                                                arg.Name, arg.ParameterType);
+                    }
+                }
+            }
+
+
+
+
+
+
+
+            Console.WriteLine("Hello User please Enter the First Name");
+            Console.WriteLine(" NOTE:- First name starts with Cap and haS nminimum 3 characters");
+            string userdata = Console.ReadLine();
+            bool value = userRegistration.validatename(userdata);
+
+            if (value)
+            {
+                Console.WriteLine("Given Name Accepted");
+
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Valid Name");
+
+            }
+
+            Console.WriteLine("Enter Last Name");
+            Console.WriteLine("NOTE:- Last name starts with Cap and has minimum 3 characters");
+
+            string userlastname = Console.ReadLine();
+
+            value = userRegistration.validatelastname(userlastname);
+
+            if (value)
+            {
+                Console.WriteLine("Given Last Name Accepted");
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Valid Last Name");
+
+            }
+
+            Console.WriteLine("Enter Mail ID");
+            Console.WriteLine("NOTE:- E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl&co) and 2 optional (xyz & in) with\r\nprecise @ and . positions");
+
+            string usermail = Console.ReadLine();
+
+            value = userRegistration.validatemail(usermail);
+
+            if (value)
+            {
+                Console.WriteLine("Given Mail ID  Accepted");
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Valid Mail ID");
+
+            }
+
+            Console.WriteLine("Enter Mobile Number");
+            Console.WriteLine("NOTE:- Mobile Format - E.g. 91 9919819801 - Country code follow by space and 10 digit number");
+
+            string usermobile = Console.ReadLine();
+
+            value = userRegistration.validatemobile(usermobile);
+
+            if (value)
+            {
+                Console.WriteLine("Given Mobile Number  Accepted");
+            }
+            else
+            {
+                Console.WriteLine("Please Enter valid Mobile number");
+
+            }
+
+                Console.WriteLine("Enter Password");
+                Console.WriteLine("NOTE:- Password minimum 8 Characters ");
+
+                string userpassword = Console.ReadLine();
+
+                value = userRegistration.validatepassword(userpassword);
+
+                if (value)
+                {
+                    Console.WriteLine("Given Password  Accepted");
+                }
+                else
+                {
+                    Console.WriteLine("Please Enter valid Password");
+
+                }
+
+
+            
+        }
+    }
+}
---
+++ Day-22-Practice-Reflection-UserRegistration/UserRegistration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day_22_Practice_Reflection_UserRegistration
+{
+    public class UserRegistration
+    {
+        public static string REGEX_NAME = "^[A-Z]{1}[a-z]{2}[a-z]*";
+        //this function is use for NAME validate
+        public bool validatename(string userdata)
+        {
+            return Regex.IsMatch(userdata, REGEX_NAME);
+        }
+
+        public static string REGEX_LASTNAME = "^[A-Z]{1}[a-z]{2}[a-z]*";
+        //this function is use for LAST NAME validate
+        public bool validatelastname(string userdata)
+        {
+            return Regex.IsMatch(userdata, REGEX_LASTNAME);
+        }
+
+        public static string REGEX_MAILID = "[a-zA-Z0-9_\\-\\.]+[@][a-z]+[\\.][a-z]{2,3}";
+        //this function is use for Mail ID validate
+        public bool validatemail(string userdata)
+        {
+            return Regex.IsMatch(userdata, REGEX_MAILID);
+        }
+
+        public static string REGEX_MOBILE = "^[9]{1}[1]{1}[ ]{1}[6-9]{1}[0-9]{9}?";
+        //this function is use for MObile Number validate
+        public bool validatemobile(string userdata)
+        {
+            return Regex.IsMatch(userdata, REGEX_MOBILE);
+        }
+
+        public static string REGEX_PASSWORD = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
+        //this function is use for Password validate
+        public bool validatepassword(string userdata)
+        {
+            return Regex.IsMatch(userdata, REGEX_PASSWORD);
+        }
+    }
+}
